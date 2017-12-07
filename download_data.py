07f9d0fd7b247a03b7a663cda159288a1d842f9a
Riley Wong <nancypwong@gmail.com>
2017-12-07 18:53:00 -0500

    Save JSON as CSV, but needs tweaking

--- download_data.py
+++ download_data.py
@@ -1,6 +1,7 @@
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import json
+import csv
 
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -22,9 +23,32 @@ playlist_ids = [
     "7pRnKuQMkmntEj7Nnj94r0"
 ]
 
-
 username = '1240951381'
 playlist_id = playlist_ids[0]
 
+### Query Spotify API
 results = sp.user_playlist(username, playlist_id)
-print(json.dumps(results, indent=4))
+json_results = json.dumps(results, indent=4)
+# print(json_results)
+data = json.loads(json_results)
+print("hey")
+print(data['tracks'])
+print(type(data['tracks']))
+
+### Download data as CSV file
+# json_parsed = json.loads(results)
+
+# Parse JSON
+save_data = open('data.csv','w')
+writer = csv.writer(save_data)
+
+count = 0
+for key, value in data.items():
+    if count == 0:
+        header = key
+        writer.writerow(header)
+        count += 1
+    writer.writerow(str(value))
+
+save_data.close()
+
